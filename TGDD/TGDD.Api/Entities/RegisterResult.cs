aa06591d8b652082b2d6a9d74e2b674eddb7c86e
ducdungbui1712 <ducdungbui1712@gmail.com>
2024-01-03 17:03:18 +0700

    Fix project conflict

--- TGDD/TGDD.Api/Entities/RegisterResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TGDD.Api.Entities
-{
-    public class RegisterResult
-    {
-        public bool Successful { get; set; }
-        public IEnumerable<string>? Errors { get; set; }
-    }
-}
